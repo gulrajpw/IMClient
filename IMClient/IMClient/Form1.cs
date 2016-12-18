bed8a322e91fa5cd629fe21e794c72ec4fd90c28
gulrajpw <gulrajpw@clarkson.edu>
2016-12-18 14:55:56 -0500

    Update Form1.cs
    Removing unused event handler

--- IMClient/IMClient/Form1.cs
+++ IMClient/IMClient/Form1.cs
@@ -138,12 +138,6 @@ public partial class Form1 : Form
         }
 
 
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            
-        }
-
         private void button3_Click(object sender, EventArgs e)
         {
             send_message();
