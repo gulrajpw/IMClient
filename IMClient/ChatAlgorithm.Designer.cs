25cb47b718e7c6913979c7c327788beb2a335403
gulrajpw <gulrajpw@clarkson.edu>
2016-12-22 19:52:13 -0500

    Updating to latest
    Removing obsolete files

---
+++ IMClient/ChatAlgorithm.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace IMClient
+{
+    partial class ChatAlgorithm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sender = new System.Windows.Forms.Button();
+            this.typeBox = new System.Windows.Forms.RichTextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.chatBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // sender
+            // 
+            this.sender.Location = new System.Drawing.Point(334, 395);
+            this.sender.Name = "sender";
+            this.sender.Size = new System.Drawing.Size(75, 23);
+            this.sender.TabIndex = 1;
+            this.sender.Text = "Send";
+            this.sender.UseVisualStyleBackColor = true;
+            this.sender.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // typeBox
+            // 
+            this.typeBox.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.typeBox.Location = new System.Drawing.Point(12, 385);
+            this.typeBox.Name = "typeBox";
+            this.typeBox.Size = new System.Drawing.Size(299, 33);
+            this.typeBox.TabIndex = 2;
+            this.typeBox.Text = "";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(334, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Username";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // chatBox
+            // 
+            this.chatBox.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.chatBox.Location = new System.Drawing.Point(12, 3);
+            this.chatBox.Multiline = true;
+            this.chatBox.Name = "chatBox";
+            this.chatBox.Size = new System.Drawing.Size(299, 367);
+            this.chatBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(421, 447);
+            this.Controls.Add(this.chatBox);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.typeBox);
+            this.Controls.Add(this.sender);
+            this.Name = "Form1";
+            this.Text = "Instant Message Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button sender;
+        private System.Windows.Forms.RichTextBox typeBox;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox chatBox;
+    }
+}
+
---
+++ IMClient/Form2.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace IMClient
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.usrNameBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Desired Username:";
+            this.label2.UseWaitCursor = true;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // usrNameBox
+            // 
+            this.usrNameBox.Location = new System.Drawing.Point(123, 62);
+            this.usrNameBox.Name = "usrNameBox";
+            this.usrNameBox.Size = new System.Drawing.Size(100, 20);
+            this.usrNameBox.TabIndex = 1;
+            this.usrNameBox.UseWaitCursor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(102, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Enter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseWaitCursor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form2
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 164);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.usrNameBox);
+            this.Controls.Add(this.label2);
+            this.Name = "Form2";
+            this.Text = "Username";
+            this.UseWaitCursor = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox usrNameBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
